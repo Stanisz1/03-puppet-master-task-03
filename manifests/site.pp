674a65d8dca3a8a7feedfd2688b6a2fca43bc0c6
Stanisz1 <34337276+Stanisz1@users.noreply.github.com>
2023-03-25 16:09:35 +0300

    Create site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,30 @@
+class httpd {
+  package { 'httpd':
+    ensure => latest
+  }  
+  service { 'httpd':
+    ensure => running,
+    enable => true,
+    require => Package['httpd']
+  }
+}
+
+
+class php {
+  package { 'php':
+    ensure => latest
+  }
+}
+
+
+node 'slave1.puppet'{
+  include httpd
+  include htmlka    
+}
+
+
+
+node 'slave2.puppet'{
+  include httpd
+  include php 
+}

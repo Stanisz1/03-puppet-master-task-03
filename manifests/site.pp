933b187c27aea4babfeda81a99e2da7c8984a613
Stanisz1 <34337276+Stanisz1@users.noreply.github.com>
2023-03-31 23:29:12 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,63 +1,57 @@
-node 'slave1' 
-
+node 'slave1' {
   class { 'html-php': }
-
   -> file {'/var/www/html/index.html':
-  ensure => 'file',
-  source => 'puppet:///modules/html/index.html',
+    ensure => 'file',
+    source => 'puppet:///modules/html/index.html',
   }
-
   -> apache::vhost { 'static':
-  port          => '80',
-  docroot       => '/var/www/html',
+    port          => '80',
+    docroot       => '/var/www/html',
   }
-
 }
 
-node 'slave2' 
-
+node 'slave2' {
   class { 'html-php': 
-  default_vhost => false,
+    default_vhost => false,
   }
-  Package { ensure => 'installed' }
-  package { 'php': }
-  package { 'libapache2-mod-php': }
-
+  package { 'php': ensure => 'installed' }
+  package { 'libapache2-mod-php': ensure => 'installed' }
   
   -> file { '/var/www/html':
-  ensure => 'directory',
+    ensure => 'directory',
   }
 
   -> file {'/var/www/html/index.php':
-  ensure => 'file',
-  source => 'puppet:///modules/php/files/index.php',
+    ensure => 'file',
+    source => 'puppet:///modules/php/files/index.php',
   }
 
   -> apache::vhost { 'dynamic':
-  port          => '80',
-  docroot       => '/var/www/html',
+    port          => '80',
+    docroot       => '/var/www/html',
   }
-
 }
 
 node 'master.puppet' {
   include nginx
 
   file { '/etc/nginx/conf.d/default.conf':
-   ensure => 'file',
-   source => 'puppet:///modules/php/files/default.conf'
+    ensure => 'file',
+    source => 'puppet:///modules/php/files/default.conf'
   }
+
   nginx::resource::server { 'static':
-   listen_port => 80,
-   proxy       => 'http://192.168.33.11:80',
+    listen_port => 80,
+    proxy       => 'http://192.168.33.11:80',
   }
+
   nginx::resource::server { 'dynamic':
-   listen_port => 80,
-   proxy       => 'http://192.168.33.12:80',
+    listen_port => 80,
+    proxy       => 'http://192.168.33.12:80',
   }
 }
 
-
 node 'mineserver.puppet'{
   include minecraft
-} 
+}
+

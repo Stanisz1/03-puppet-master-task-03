c2524c649a74d023c931055ef36608fc7fd36709
Stanislaw <atrohov97@gmail.com>
2023-04-01 01:12:47 +0300

    C:/Program Files/Git/

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,31 +1,17 @@
-node slave1 {
-  package { 'httpd':
-    ensure => installed,
-    name   => httpd,
+node 'slave1.puppet' {
+   class { 'apache': }
   }
   file { '/var/www/html/index.html':
     ensure => present,
     source => "puppet:///modules/html/index.html",
   }
-  service { 'httpd':
-    ensure => running,
-    enable => true,
-  }
-  service { 'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
-}
+ 
 
-node slave2 {
-  package { 'httpd':
-    ensure => installed,
-    name   => httpd,
-  }
-  package { 'php':
-    ensure => installed,
-    name   => php,
-  }
+ode 'slave2.puppet' {
+   class { 'apache::mod::php': }
+
+   class { 'php': }
+
   file { '/var/www/html/index.php':
     ensure => present,
     source => "puppet:///modules/php/files/index.php",
@@ -44,33 +30,66 @@ node slave2 {
   }
 }
 
-node master {
-  service { 'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
-  
-  include nginx
+node 'master.puppet' {
 
-  nginx::resource::server { '192.168.33.10':
-    listen_port => 80,
-    proxy => 'http://192.168.33.11',
+include nginx
+
+nginx::resource::server { 'static':
+  listen_port => 80,
+  proxy => 'http://192.168.33.11:80',
   }
-  nginx::resource::server { '192.168.33.10:81':
-    listen_port => 81,
-    proxy => 'http://192.168.33.12',
+
+nginx::resource::server { 'dynamic':
+  listen_port => 81,
+  proxy => 'http://192.168.33.12:80',
   }
-  exec { 'config SELinux Booleans':
-    command => 'setsebool -P httpd_can_network_connect on',
-    path    => "/usr/sbin",
+
+
+exec { 'selinux_to_permissive':
+  command     => 'setenforce 0',
+  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+  user       => 'root',
+  }
+
+exec { 'reboot_nginx':
+  command     => 'systemctl restart nginx',
+  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+  user => 'root',
   }
 }
 
-node mineserver.puppet {
-  service { 'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
-  
-  include minecraft
+node 'mineserver.puppet' {
+
+ package {'java-17-openjdk':
+  ensure => installed,
+}
+
+ file {'/opt/minecraft':
+  ensure => directory,
+}
+
+ file {'/opt/minecraft/eula.txt':
+  content => 'eula=true',
+}
+
+file { '/opt/minecraft/server.jar':
+  ensure => file,
+  source => 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar',
+  replace => false,
+     }
+
+  file { '/etc/systemd/system/minecraft.service':
+    owner => 'root',
+    group => 'root',
+    ensure => file,
+    mode   => '0644',
+    source => 'puppet:///modules/maincraft/files/service',
+    replace => false,
+    }
+
+
+ ~> service { 'minecraft':
+        ensure => running,
+        enable => true
+   }
 }
--- modules/maincraft/manifests/init.pp
+++ modules/maincraft/manifests/init.pp
@@ -1,30 +1,59 @@
 class minecraft {
-  Package { ensure => 'installed' }
-  package { 'default-jre': }
-  package { 'default-jdk': }
-  
-  file {'/opt/minecraft':
-  ensure => directory,
+  package {  'java':
+    name   => java-17-openjdk,
+    ensure => present,
   }
-
-  include wget
-  wget::retrieve { 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar':
-  destination => '/opt/minecraft/',
+  file { '/opt/minecraft':
+    ensure => directory,
   }
-
-  file { '/etc/systemd/system/minecraft.service':
-   ensure => 'file',
-   source => 'puppet:///modules/minecraft/service'
+  wget::retrieve { 'download minecraft service':
+    source      => 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar',
+    destination => '/opt/minecraft/',
+    timeout     => 0,
+    verbose     => false,
+    require => File['/opt/minecraft'],  
   }
-
+  exec { 'init start service':
+    cwd     => '/opt/minecraft',
+    command => 'java -Xmx1024M -Xms1024M -jar service.jar --nogui',
+    path    => "/usr/bin",
+    unless  => 'test -e /opt/minecraft_2/eula.txt',
+  }
   file { '/opt/minecraft/eula.txt':
-   ensure => 'file',
-   source => 'puppet:///modules/minecraft/eula.txt'
-  } 
-
-  ~> service { 'minecraft.service':
-      ensure => running,
-      enable => true,
+    content => "eula=true",
+    require => Exec['init start service'],
+  }  
+  vcsrepo { '/opt/minecraft/mcrcon':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/Tiiffi/mcrcon.git',
+    require => File['/opt/minecraft'],
+  }
+  exec { 'make mcrcon':
+    cwd     => '/opt/minecraft/mcrcon',
+    command => 'make',
+    path    => "/usr/bin",
+    unless  => 'test -x /opt/minecraft/mcrcon/mcrcon',
+  }
+  exec { 'install mcrcon':
+    cwd     => '/opt/minecraft/mcrcon',
+    path    => "/usr/bin",
+    command => 'make install',
+    unless  => 'test -x /usr/local/bin/mcrcon',   
+  }
+  file { '/opt/minecraft/service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/files/service',
+  }
+  file { '/etc/systemd/system/service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/files/service',
+    require => File['/opt/minecraft/service.properties'],    
+  }
+  service { 'service':
+    ensure => running,
+    enable => true,
+    require => File['/etc/systemd/system/service'],
   }
 }
    

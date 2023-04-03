078fa1308f711b2b1f6df88407c66ec7772add4e
Stanislaw <atrohov97@gmail.com>
2023-04-03 23:29:04 +0300

    ....

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,42 +1,64 @@
-node 'slave1.puppet'{
-  package {'httpd':
-    ensure => installed,
-}
-  
-  service {'firewalld':
+node master.puppet {
+  service { 'firewalld':
     ensure => stopped,
-}
-  
-  file { '/var/www/html/index.html':
-    ensure => file,
-    source => '/vagrant/index.html'
-}
-   
-  service {'httpd':
-    ensure => running,
-}
+    enable => false,
+  }
+  include nginx
+  nginx::resource::server { 'static':
+    listen_port => 80,
+    proxy => 'http://192.168.33.11',
+  }
+  nginx::resource::server { 'dynamic':
+    listen_port => 81,
+    proxy => 'http://192.168.33.12',
+  }
+  exec { 'config SELinux Booleans':
+    command => 'setsebool -P httpd_can_network_connect on',
+    path    => "/usr/sbin",
+  }
 }
 
-node 'slave2.puppet'{
-  package {'httpd':
+node slave1.puppet {
+  package { 'httpd':
     ensure => installed,
-}
-  
-  package {'php':
-    ensure => installed,
-}
-  
-  service {'firewalld':
-    ensure => stopped,
-} 
-  file {'/var/www/html/index.php':
-    ensure => file,
-    source => '/vagrant/index.php'
-}
-  
-  service {'httpd':
+  }
+  file { '/var/www/html/index.html':
+    ensure => present,
+    source => "/vagrant/index.html",
+  }
+  service { 'httpd':
     ensure => running,
-}  
+    enable => true,
+  }
+  service { 'firewalld':
+    ensure => stopped,
+    enable => false,
+  }
+}
+
+node slave2.puppet {
+  package { 'httpd':
+    ensure => installed,
+  }
+  package { 'php':
+    ensure => installed,
+  }
+  file { '/var/www/html/index.php':
+    ensure => present,
+    source => "/vagrant/index.php",
+  }
+  service { 'php-fpm':
+    ensure => running,
+    enable => true,
+  }
+  service { 'httpd':
+    ensure => running,
+    enable => true,
+  }
+  service { 'firewalld':
+    ensure => stopped,
+    enable => false,
+  }
 }
 
 node mineserver.puppet {
@@ -45,30 +67,4 @@ node mineserver.puppet {
     enable => false,
   }
 include minecraft
-}
-
-node 'master.puppet'{
-
-  service { 'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
-
-  include nginx
-  
-  nginx::resource::server {'static':
-    listen_port => 80,
-    proxy       => 'http://192.168.33.11',
-}
-
-  nginx::resource::server {'dynamic':
-    listen_port => 81,
-    proxy       => 'http://192.168.33.12',
-}
-
-
- exec { 'config SELinux Booleans':
-    command => 'setsebool -P httpd_can_network_connect on',
-    path    => "/usr/sbin",
-  }
 }

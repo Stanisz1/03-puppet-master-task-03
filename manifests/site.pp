6f1d5a57c3a9875749eb1536493551a6d461ff20
Stanislaw <atrohov97@gmail.com>
2023-04-03 18:44:40 +0300

    ....

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,85 +14,72 @@ nginx::resource::server { 'dynamic':
   }
 
 
-exec { 'selinux_to_permissive':
-  command     => 'setenforce 0',
-  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
-  user       => 'root',
-  }
-
-exec { 'reboot_nginx':
-  command     => 'systemctl restart nginx',
-  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
-  user => 'root',
-  }
+class { selinux:
+    mode => 'permissive',
+    type => 'targeted',
 }
 
-node 'slave1.puppet' {
-   class { 'apache': }
-  }
+  exec {'restart_nginx':
+    command     => 'systemctl restart nginx',
+    path        => ['/bin'],
+    user => 'root',
+} 
+}
+
+node 'slave1.puppet'{
+  package {'httpd':
+    ensure => installed,
+}
+  
+  service {'firewalld':
+    ensure => stopped,
+}
+  
   file { '/var/www/html/index.html':
-    ensure => present,
-    source => "puppet:///modules/html/index.html",
-  }
+    ensure => file,
+    source => 'puppet:///modules/html/index.html'
+}
+   
+  service {'httpd':
+    ensure => running,
+}
+}
+
  
 
-ode 'slave2.puppet' {
-   class { 'apache::mod::php': }
-
-   class { 'php': }
-
-  file { '/var/www/html/index.php':
-    ensure => present,
-    source => "puppet:///modules/php/files/index.php",
-  }
-  service { 'php-fpm':
-    ensure => running,
-    enable => true,
-  }
-  service { 'httpd':
-    ensure => running,
-    enable => true,
-  }
-  service { 'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
+node 'slave2.puppet'{
+  package {'httpd':
+    ensure => installed,
 }
-
+  
+  package {'php':
+    ensure => installed,
+}
+  
+  service {'firewalld':
+    ensure => stopped,
+} 
+  file {'/var/www/html/index.php':
+    ensure => file,
+    source => 'puppet:///modules/php/files/index.php'
+}
+  
+  service {'httpd':
+    ensure => running,
+}  
+}
 
 
 node 'mineserver.puppet' {
 
- package {'java-17-openjdk':
-  ensure => installed,
+ service {'firewalld':
+    ensure => stopped,
+} 
+
+  class { selinux:
+    mode => 'permissive',
+    type => 'targeted',
 }
-
- file {'/opt/minecraft':
-  ensure => directory,
-}
-
- file {'/opt/minecraft/eula.txt':
-  content => 'eula=true',
-}
-
-file { '/opt/minecraft/server.jar':
-  ensure => file,
-  source => 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar',
-  replace => false,
-     }
-
-  file { '/etc/systemd/system/minecraft.service':
-    owner => 'root',
-    group => 'root',
-    ensure => file,
-    mode   => '0644',
-    source => 'puppet:///modules/maincraft/files/minecraft.service',
-    replace => false,
-    }
-
-
- ~> service { 'minecraft':
-        ensure => running,
-        enable => true
-   }
+  
+  include minecraft 
 }

3f2b7ccaf33fb010225eb775813acf66f4b5e6ab
Stanisz1 <34337276+Stanisz1@users.noreply.github.com>
2023-06-19 17:22:09 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,73 +1,80 @@
-node 'slave1.puppet' {
-  package { 'httpd':
+node 'slave1.puppet'{
+  package {'httpd':
     ensure => installed,
-  }
+}
+  
+  service {'firewalld':
+    ensure => stopped,
+}
+  
   file { '/var/www/html/index.html':
-    ensure => present,
-    source => '/vagrant/index.html',
-  }
-  service { 'httpd':
+    ensure => file,
+    source => '/vagrant/index.html'
+}
+   
+  service {'httpd':
     ensure => running,
-    enable => true,
-  }
-  service { 'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
+}
 }
 
-node 'slave2.puppet' {
-  package { 'httpd':
+node 'slave2.puppet'{
+  package {'httpd':
     ensure => installed,
-  }
-  package { 'php':
+}
+  
+  package {'php':
     ensure => installed,
-  }
-  file { '/var/www/html/index.php':
-    ensure => present,
-    source => '/vagrant/index.php',
-  }
-  service { 'php-fpm':
-    ensure => running,
-    enable => true,
-  }
-  service { 'httpd':
-    ensure => running,
-    enable => true,
-  }
-  service { 'firewalld':
+}
+  
+  service {'firewalld':
     ensure => stopped,
-    enable => false,
-  }
+} 
+  file {'/var/www/html/index.php':
+    ensure => file,
+    source => '/vagrant/index.php'
+}
+  
+  service {'httpd':
+    ensure => running,
+}  
 }
 
-node 'mineserver.puppet' {
-  service { 'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
+node 'mineserver.puppet'{
 
-  include minecraft
+  service {'firewalld':
+    ensure => stopped,
+} 
+
+  class { selinux:
+    mode => 'permissive',
+    type => 'targeted',
+}
+  
+  include minecraft 
 }
 
-node 'master.puppet' {
-  service { 'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
+node 'master.puppet'{
 
   include nginx
-
-  nginx::resource::server { 'static':
-    listen_port => 80,
-    proxy       => 'http://192.168.33.11',
-  }
-  nginx::resource::server { 'dynamic':
+  
+  nginx::resource::server {'static':
     listen_port => 81,
-    proxy       => 'http://192.168.33.12',
-  }
-  exec { 'config SELinux Booleans':
-    command => 'setsebool -P httpd_can_network_connect on',
-    path    => '/usr/sbin',
-  }
+    proxy       => 'http://192.168.50.11:80',
+}
+
+  nginx::resource::server {'dynamic':
+    listen_port => 8080,
+    proxy       => 'http://192.168.50.12:80',
+}
+
+  class { selinux:
+    mode => 'permissive',
+    type => 'targeted',
+}
+
+  exec {'restart_nginx':
+    command     => 'systemctl restart nginx',
+    path        => ['/bin'],
+    user => 'root',
+} 
 }
